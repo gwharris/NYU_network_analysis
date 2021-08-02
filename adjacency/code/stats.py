486e56a3493e25729449a5dd6794bffb15829139
instagr4h4m <grahamh1019@gmail.com>
2021-08-02 13:20:20 -0400

    Create stats.py

---
+++ adjacency/code/stats.py
@@ -0,0 +1,58 @@
+"""
+Graham Harris
+Summer Data Project
+Calculates numerical information about adjacency lists.
+"""
+
+import sys
+
+# -------------- METHODS --------------
+
+# Converts CSV to a 2D/nested Python list structure.
+def csv_to_list(path):
+  fd = open(path, "r")
+  fd.readline() # get rid of title line
+  rc = []
+  for line in fd:
+    line = line.lower() # make sure all casing is the same
+    line = line.split(",")
+    rc.append(line)
+  fd.close()
+  return rc
+
+try:
+  read = sys.argv[1] # Get file name
+  lines = csv_to_list("../data/" + read + ".csv")
+
+  # Create 2 empty list of events
+  single_event = {line[1]:0 for line in lines}
+  multiple_event = {line[1]:0 for line in lines}
+
+  # Get num of edges per person and list of events
+  all_attendees = {}
+  for line in lines:
+    if line[0] not in all_attendees:
+      all_attendees[line[0]] = 1
+    else:
+      all_attendees[line[0]] += 1
+  
+  # Eliminate anyone with more than 1 edge
+  # Only 1 edge means only 1 program attendance
+  single_attendees = {k:v for k,v in all_attendees.items() if v == 1}
+  
+  for line in lines:
+    if line[0] in single_attendees:
+      print(line[1])
+      single_event[line[1]] += 1
+    multiple_event[line[1]] += 1
+  
+  print(multiple_event)
+  print(single_event)
+
+  # Write numbers to file
+  fd = open("../stats/" + read + ".txt", "w")
+  for e in single_event:
+    fd.write(e + ": " + str(single_event[e]) + " people (" + str((single_event[e]/multiple_event[e]) * 100) + "%)\n")
+
+except:
+  print("oops")
