6cb9ba5cd5695d95c7f8db70d760e19d5101d216
instagr4h4m <grahamh1019@gmail.com>
2021-08-24 13:06:32 -0400

    Change file to allow multiple arguments

--- adjacency/code/visual.py
+++ adjacency/code/visual.py
@@ -11,39 +11,38 @@ import sys
 
 # -------------- MAIN --------------
 
-try:
-  line = sys.argv[1] # Get file name
+for arg in sys.argv:
+  try:
+    df = pd.read_csv("../data/" + arg + ".csv")
+    print("\nFile found: " + arg)
 
-  df = pd.read_csv("../data/" + line + ".csv")
-  print("\nFile found")
+    net = Network(notebook=True, height="750px", width="75%")
+    net.force_atlas_2based()
+    net.show_buttons(filter_="physics")
 
-  net = Network(notebook=True, height="750px", width="75%")
-  net.force_atlas_2based()
-  net.show_buttons(filter_="physics")
+    source = df['Source']
+    target = df['Target']
+    weight = df['Weight']
 
-  source = df['Source']
-  target = df['Target']
-  weight = df['Weight']
+    edges = zip(source, target, weight)
 
-  edges = zip(source, target, weight)
-
-  # Simplified Teams
-  print("\nWriting nodes and edges...\n")
-  for src, dst, wgt in edges:
-    # Add nodes and edges to the graph
-    net.add_node(src, src, title=src, color='#D4D4D4')
-    if "Applications" in dst:
-      net.add_node(dst, dst, title=dst, color='#018b95')
-      net.add_edge(src, dst, color='#019ca8')
-    elif "Participant" in dst:
-      net.add_node(dst, dst, title=dst, color='#7908C4')
-      net.add_edge(src, dst, color="#9007EB")
-    else:
-      net.add_node(dst, dst, title=dst, color='#707070')
-      net.add_edge(src, dst, color="#707070")
+    # Simplified Teams
+    print("Writing nodes and edges...")
+    for src, dst, wgt in edges:
+      # Add nodes and edges to the graph
+      net.add_node(src, src, title=src, color='#D4D4D4')
+      if "Applications" in dst:
+        net.add_node(dst, dst, title=dst, color='#018b95')
+        net.add_edge(src, dst, color='#019ca8')
+      elif "Participant" in dst:
+        net.add_node(dst, dst, title=dst, color='#7908C4')
+        net.add_edge(src, dst, color="#9007EB")
+      else:
+        net.add_node(dst, dst, title=dst, color='#707070')
+        net.add_edge(src, dst, color="#707070")
   
-  # net.show_buttons(filter_=['physics', 'nodes', 'edges'])
-  net.show("../html/" + line + ".html")
+    # net.show_buttons(filter_=['physics', 'nodes', 'edges'])
+    net.show("../html/" + arg + ".html")
 
-except:
-  print("\nunable to read argument 1\n")
+  except:
+    print("\nunable to read argument: " + arg)
