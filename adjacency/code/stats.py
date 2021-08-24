9b1ee54c66bf3f02fde8949103ea6032e607bedf
instagr4h4m <grahamh1019@gmail.com>
2021-08-24 13:20:14 -0400

    Allow for multiple arguments

--- adjacency/code/stats.py
+++ adjacency/code/stats.py
@@ -20,40 +20,42 @@ def csv_to_list(path):
   fd.close()
   return rc
 
-try:
-  read = sys.argv[1] # Get file name
-  lines = csv_to_list("../data/" + read + ".csv")
-  lines_fixed = []
-  [lines_fixed.append(line) for line in lines if line not in lines_fixed]
+for arg in sys.argv:
+  try:
+    lines = csv_to_list("../data/" + arg + ".csv")
+    print("\nFile found: " + arg)
 
-  # Create 2 empty list of events
-  single_event = {line[1]:0 for line in lines_fixed}
-  multiple_event = {line[1]:0 for line in lines_fixed}
+    lines_fixed = []
+    [lines_fixed.append(line) for line in lines if line not in lines_fixed] # eliminate duplicates
 
-  # Get num of edges per person and list of events
-  all_attendees = {}
-  for line in lines_fixed:
-    if line[0] not in all_attendees:
-      all_attendees[line[0]] = 1
-    else:
-      all_attendees[line[0]] += 1
+    # Create 2 empty list of events
+    single_event = {line[1]:0 for line in lines_fixed}
+    multiple_event = {line[1]:0 for line in lines_fixed}
+
+    # Get num of edges per person and list of events
+    all_attendees = {}
+    for line in lines_fixed:
+      if line[0] not in all_attendees:
+        all_attendees[line[0]] = 1
+      else:
+        all_attendees[line[0]] += 1
   
-  # Eliminate anyone with more than 1 edge
-  # Only 1 edge means only 1 program attendance
-  single_attendees = {k:v for k,v in all_attendees.items() if v == 1}
+    # Eliminate anyone with more than 1 edge
+    # Only 1 edge means only 1 program attendance
+    single_attendees = {k:v for k,v in all_attendees.items() if v == 1}
   
-  # Counters
-  for line in lines_fixed:
-    if line[0] in single_attendees:
-      single_event[line[1]] += 1
-    multiple_event[line[1]] += 1
+    # Counters
+    for line in lines_fixed:
+      if line[0] in single_attendees:
+        single_event[line[1]] += 1
+      multiple_event[line[1]] += 1
 
-  # Write numbers to file
-  fd = open("../stats/" + read + ".txt", "w")
-  for e in single_event:
-    fd.write(e + ": " + str(single_event[e]) + " people (" + str((single_event[e]/multiple_event[e]) * 100) + "%)\n")
-  
-  print("success")
+    # Write numbers to file
+    fd = open("../stats/" + arg + ".txt", "w")
+    for e in single_event:
+      fd.write(e + ": " + str(single_event[e]) + " people (" + str((single_event[e]/multiple_event[e]) * 100) + "%)\n")
 
-except:
-  print("oops")
+    print("Success")
+
+  except:
+    print("\n\tFailure reading argument: " + arg)
